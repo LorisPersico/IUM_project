3d7ff43020a44eab58a23ca38ce316528d6cfe71
Kilback <loris.persico@edu.unito.it>
2024-01-09 18:46:40 +0100

    Prova route games serverMongoDB

---
+++ server-mongodb/databases/database.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+//The URL which will be queried. Run "mongod.exe" for this to connect
+//var url = 'mongodb://localhost:27017/test';
+const mongoDB = 'mongodb://localhost:27017/football_data';
+mongoose.Promise = global.Promise;
+connection = mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    checkServerIdentity: false,
+    family: 4
+})
+    .then(() => {
+            console.log('connection to mongodb worked!');
+    })
+    .catch((error) => {
+        console.log('connection to mongodb did not work! ' + JSON.stringify(error));
+    });
+
+const gameSchema = new mongoose.Schema({
+});
+
+
+const Game = mongoose.model('Game', gameSchema);
+
+module.exports = { Game };
+
+
--- server-mongodb/routes/index.js
+++ server-mongodb/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const {Game} = require("../databases/database");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/games', async (req, res) => {
+  try {
+    const games = await Game.find();
+    res.json(games);
+  } catch (error) {
+    console.error('Errore nel recupero delle partite:', error);
+    res.status(500).json({ error: 'Errore nel recupero delle partite' });
+  }
+});
+
 
 module.exports = router;
